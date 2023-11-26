6053e12564ac1fe4612d10c73d9668ad5fffc98f
Jeremy Delley <jeremy.delley@imex-dtp.com>
2023-11-26 22:21:22 +0100

    testing Walkdir

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,13 @@
 use walkdir::WalkDir;
 
 pub fn list_files() {
-    for entry in WalkDir::new(".").into_iter().filter_map(|e| e.ok()) {
-        println!("{}", entry.path().display());
+    for entry in WalkDir::new(".")
+        .into_iter()
+        .filter_map(|e| e.ok())
+        .filter(|f| f.file_type().is_file())
+    {
+        if entry.file_type().is_file() {
+            println!("{}", entry.path().display());
+        }
     }
 }
