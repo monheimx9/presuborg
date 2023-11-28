a11feec11af2180fbe951380a920a3b6ea21cd83
Jeremy Delley <jeremy.delley@imex-dtp.com>
2023-11-28 20:42:09 +0100

    designing things

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,35 @@
+use std::collections::HashMap;
+use std::fs;
+use std::io::Read;
 use std::path::{Path, PathBuf};
 use walkdir::WalkDir;
 
+use regex::{self, Regex};
+use serde::{Deserialize, Serialize};
+use sevenz_rust::decompress_file;
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Language {
+    ext: Vec<String>,
+    name: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct LanguageMap(HashMap<String, Language>);
+
+impl LanguageMap {
+    fn find_language_key(&self, extension: &str) -> Option<&str> {
+        for (key, language) in self.0.iter() {
+            if language.ext.contains(&extension.to_lowercase()) {
+                return Some(key);
+            }
+        }
+        None
+    }
+    fn get_track_name(&self, key: &str) -> Option<&str> {
+        self.0.get(key).map(|lang| lang.name.as_str())
+    }
+}
 struct Episode {
     number: u32,
     lang: String,
@@ -24,17 +53,28 @@ pub fn list_subtitles() -> Vec<PathBuf> {
 }
 
 fn parse_subtitles_into_episodes(pb: &[PathBuf]) -> Vec<Episode> {
+    let langs = get_languages();
+    for pe in pb {
+        let lang = get_lang_flag_from_filename(pe, &langs);
+    }
     unimplemented!()
 }
 
-fn get_lang_flag_from_filename(p: &Path) -> &str {
+fn get_lang_flag_from_filename<'a>(p: &Path, langs: &'a LanguageMap) -> &'a str {
     unimplemented!()
 }
 
-pub fn group_by_extension(pb: &[PathBuf]) -> Vec<Vec<PathBuf>> {
-    unimplemented!();
+fn get_episode_number_from_filename(fname: &str) -> u32 {
+    let re = Regex::new(r".+(\s|S\d{0,2}E)?(\d{2})\s?.+").unwrap();
+    let research = re.captures(fname).unwrap();
+    let epnum: u32 = research[1].parse().unwrap();
+    epnum
 }
 
-pub fn group_by_lang() {
-    unimplemented!();
+fn get_languages() -> LanguageMap {
+    let mut file = fs::File::open("langs.json").expect("Failed to open langs.json");
+    let mut json_data = String::new();
+    file.read_to_string(&mut json_data)
+        .expect("Faile to read json string data");
+    LanguageMap(serde_json::from_str(&json_data).expect("Failed to parse json"))
 }
